a232c217e4c0ad9a354e12b3b1e9a3fcb63f38c4
artibilov <artibilov@googlemail.com>
2026-05-29 16:22:29 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,25 +2,34 @@ const markdownIt = require("markdown-it");
 const makeWikilinks = require("markdown-it-wikilinks");
 
 module.exports = function(eleventyConfig) {
-  // Безопасное динамическое вычисление путей
+  // Настройка путей для файлов (Eleventy раскладывает их по красивым папкам)
   eleventyConfig.addGlobalData("permalink", (data) => {
-    // Если данные страницы еще не инициализированы, возвращаем undefined
     if (!data || !data.page) {
       return undefined; 
     }
-    // Если в YAML-заголовке заметки жестко прописан permalink (как у index.md), берем его
     if (data.permalink) {
       return data.permalink;
     }
-    // Для всех остальных файлов убираем префикс папки плагина и добавляем .html
-    return data.page.filePathStem.replace("/src/site/notes/", "") + ".html";
+    return undefined; 
   });
 
   const wikilinks = makeWikilinks({
     baseURL: "/digital-garden/",
     relativeURLs: false,
     makeUrl: (name) => {
-      return "/digital-garden/" + name.toLowerCase().replace(/ /g, "-") + ".html";
+      // 1. Делаем имя строчным
+      let slug = name.toLowerCase().trim();
+      
+      // 2. Если плагин передал путь вместе с папкой "sense/", убираем этот префикс, чтобы не дублировать
+      slug = slug.replace(/^sense\//, "");
+      
+      // 3. Очищаем от специфических символов, кавычек и знаков препинания
+      slug = slug
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()«»"']/g, "")
+        .replace(/\s+/g, "-"); // заменяем пробелы на дефисы
+      
+      // 4. Возвращаем строгий абсолютный путь от корня домена Гитхаба
+      return "/digital-garden/sense/" + slug + "/";
     }
   });
 
