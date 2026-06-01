c3758b52c5bc2f05ab6d89cf44ed9e2260a8932c
artibilov <artibilov@googlemail.com>
2026-06-01 08:54:21 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -60,7 +60,7 @@ module.exports = function(eleventyConfig) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${pageTitle}</title>
-   <link rel="stylesheet" href="/digital-garden/style.css">
+   <link rel="stylesheet" href="https://artibilov.github.io/digital-garden/style.css">
 <body>
     <div class="container">
         ${content}
